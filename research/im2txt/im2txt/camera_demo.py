8ae4b1849462a4ec8a3f46dc29ae3031bf28cd8a
Aghasy <agasy18@gmail.com>
2018-09-11 23:59:44 +0400

    add object detecor to camera demo

--- research/im2txt/im2txt/camera_demo.py
+++ research/im2txt/im2txt/camera_demo.py
@@ -1,6 +1,23 @@
+###
+# build:
+# bazel build -c opt //im2txt/...
+# pushd ..; protoc object_detection/protos/*.proto --python_out=.; popd
+# example:
+# bazel-bin/im2txt/camera_demo /Users/aghasy/GoogleDrive/SCI/PHD/demo/im2txt_orig/obj+const_model.ckpt-1000000.pb /Users/aghasy/GoogleDrive/SCI/PHD/demo/im2txt_orig/words.txt
+###
+
+
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import os
+import sys
+
+sys.path.insert(0, '..')
+sys.path.insert(0, '../slim/')
+sys.path.insert(0, '../object_detection')
 
 import math
 from time import time
@@ -8,14 +25,29 @@ import numpy as np
 import argparse
 import cv2
 import tensorflow as tf
+
 from im2txt.inference_utils import caption_generator
 from im2txt.inference_utils import vocabulary
+from object_detection.utils import ops as utils_ops
+from utils import label_map_util
+from utils import visualization_utils as vis_util
 
 parser = argparse.ArgumentParser()
 parser.add_argument('model')
 parser.add_argument('vocab_file')
+parser.add_argument('--path_to_labels', default='../object_detection/data/mscoco_label_map.pbtxt')
+parser.add_argument('--num_classes', type=int, default=90)
 
 args = parser.parse_args()
+
+PATH_TO_LABELS = os.path.abspath(args.path_to_labels)
+NUM_CLASSES = args.num_classes
+
+
+label_map  = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
 
 
 class InferenceWrapper:
@@ -24,11 +56,21 @@ class InferenceWrapper:
     with tf.gfile.FastGFile(model_path, "rb") as f:
       graph_def.ParseFromString(f.read())
     self.image_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, None, None, 3))
+    self.results = {}
     tf.import_graph_def(graph_def, name="", input_map={'ExpandDims_1': self.image_placeholder})
 
   def feed_image(self, sess, encoded_image):
-    initial_state = sess.run(fetches="lstm/initial_state:0",
-                             feed_dict={self.image_placeholder: encoded_image})
+    fetches = [
+      'lstm/initial_state',
+      'num_detections',
+      'detection_boxes',
+      'detection_scores',
+      'detection_classes'
+    ]
+
+    self.results = sess.run(fetches=dict(zip(fetches, [f + ':0' for f in fetches])),
+                            feed_dict={self.image_placeholder: encoded_image})
+    initial_state = self.results['lstm/initial_state']
     return initial_state
 
   def inference_step(self, sess, input_feed, state_feed):
@@ -55,6 +97,7 @@ with tf.Session() as sess:
     t0 = time()
     ret, frame = cap.read()
     frame_f = (np.expand_dims(frame, 0) / 255.0).astype(np.float32)
+
     captions = generator.beam_search(sess, frame_f)
 
     last_fps += 1.0 / (time() - t0)
@@ -75,6 +118,16 @@ with tf.Session() as sess:
     spaceing = 50
 
     sp = 0
+
+    frame = vis_util.visualize_boxes_and_labels_on_image_array(
+      frame,
+      model.results['detection_boxes'][0],
+      model.results['detection_classes'][0].astype(np.uint8),
+      model.results['detection_scores'][0],
+      category_index,
+      instance_masks=None,
+      use_normalized_coordinates=True,
+      line_thickness=min(frame.shape[:-1]) // 100)
 
     sp = max(sp, sum(p for c, p in captions))
     for i, (c, p) in enumerate(captions):
