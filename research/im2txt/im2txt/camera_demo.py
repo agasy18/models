5f888f175e40aa5794cec3f14dcb469b42c114a1
Aghasy <agasy18@gmail.com>
2018-11-12 04:48:19 +0400

    add congis

--- research/im2txt/im2txt/camera_demo.py
+++ research/im2txt/im2txt/camera_demo.py
@@ -31,17 +31,31 @@ from im2txt.inference_utils import vocabulary
 from object_detection.utils import ops as utils_ops
 from utils import label_map_util
 from utils import visualization_utils as vis_util
+import json
+from sys import exit
 
 parser = argparse.ArgumentParser()
-parser.add_argument('model')
-parser.add_argument('vocab_file')
-parser.add_argument('--path_to_labels', default='../object_detection/data/mscoco_label_map.pbtxt')
-parser.add_argument('--num_classes', type=int, default=90)
+parser.add_argument('config')
 
 args = parser.parse_args()
+with open(args.config) as f:
+  config = json.load(f)
+      
 
-PATH_TO_LABELS = os.path.abspath(args.path_to_labels)
-NUM_CLASSES = args.num_classes
+model_input_mapping = config['model']['input_mapping']  
+model_output_mapping = config['model']['output_mapping']  
+
+def abs_path(path):
+    if os.path.exists(path):
+          return os.path.abspath(path)
+    path2 = os.path.join(os.path.dirname(args.config), path)
+    if os.path.exists(path2):
+          return os.path.abspath(path2)
+    exit('File not found: ' + path)
+
+
+PATH_TO_LABELS = abs_path(config['path_to_labels'])
+NUM_CLASSES = config['num_classes']
 
 
 label_map  = label_map_util.load_labelmap(PATH_TO_LABELS)
@@ -57,41 +71,34 @@ class InferenceWrapper:
       graph_def.ParseFromString(f.read())
     self.image_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, None, None, 3))
     self.results = {}
-    for name in ['ExpandDims_1', 'ExpandDims_3']:
-      try:
-        tf.import_graph_def(graph_def, name="", input_map={name: self.image_placeholder})
-        break
-      except ValueError as e:
-        if name in str(e):
-              continue
-        raise e
+    tf.import_graph_def(graph_def, name="", input_map={model_input_mapping['float_image']: self.image_placeholder})
 
   def feed_image(self, sess, encoded_image):
     fetches = [
-      'lstm/initial_state',
-      'num_detections',
-      'detection_boxes',
-      'detection_scores',
-      'detection_classes'
+      model_output_mapping['lstm/initial_state'],
+      model_output_mapping['num_detections'],
+      model_output_mapping['detection_boxes'],
+      model_output_mapping['detection_scores'],
+      model_output_mapping['detection_classes']
     ]
 
     self.results = sess.run(fetches=dict(zip(fetches, [f + ':0' for f in fetches])),
                             feed_dict={self.image_placeholder: encoded_image})
-    initial_state = self.results['lstm/initial_state']
+    initial_state = self.results[model_output_mapping['lstm/initial_state']]
     return initial_state
 
   def inference_step(self, sess, input_feed, state_feed):
     softmax_output, state_output = sess.run(
-      fetches=["softmax:0", "lstm/state:0"],
+      fetches=[model_output_mapping["softmax"] + ":0", model_output_mapping["lstm/state"] + ":0"],
       feed_dict={
-        "input_feed:0": input_feed,
-        "lstm/state_feed:0": state_feed,
+        model_input_mapping["input_feed"] + ":0": input_feed,
+        model_input_mapping["lstm/state_feed"] + ":0": state_feed,
       })
     return softmax_output, state_output, None
 
 
-vocab = vocabulary.Vocabulary(args.vocab_file)
-model = InferenceWrapper(args.model)
+vocab = vocabulary.Vocabulary(abs_path(config['vocab_file']))
+model = InferenceWrapper(abs_path(config['model']['path']))
 
 with tf.Session() as sess:
   generator = caption_generator.CaptionGenerator(model, vocab)
@@ -130,9 +137,9 @@ with tf.Session() as sess:
 
     frame = vis_util.visualize_boxes_and_labels_on_image_array(
       frame,
-      model.results['detection_boxes'][0],
-      model.results['detection_classes'][0].astype(np.uint8),
-      model.results['detection_scores'][0],
+      model.results[model_output_mapping['detection_boxes']][0],
+      model.results[model_output_mapping['detection_classes']][0].astype(np.uint8),
+      model.results[model_output_mapping['detection_scores']][0],
       category_index,
       instance_masks=None,
       use_normalized_coordinates=True,
