b89979a270a0edd5a8423e81b3109d98ac78ad67
Aghasy <agasy18@gmail.com>
2019-03-27 21:07:05 +0400

    add timer

--- research/im2txt/im2txt/camera_demo.py
+++ research/im2txt/im2txt/camera_demo.py
@@ -67,10 +67,54 @@ categories = label_map_util.convert_label_map_to_categories(label_map, max_num_c
 category_index = label_map_util.create_category_index(categories)
 
 
+class CodeBlockTimer:
+    def __init__(self, auto_reset=-1):
+        self.count = 0
+        self.totalDuration = 0.0
+        self.maxTime = 0
+        self.minTime = 0
+        self._enter_time = 0
+        self.auto_reset = auto_reset
+
+    def __enter__(self):
+        if 0 < self.auto_reset < self.count:
+            self.reset()
+
+        self._enter_time = time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.count += 1
+        t = time() - self._enter_time
+        self.maxTime = max(t, self.maxTime)
+        self.minTime = min(t, self.minTime)
+        self.totalDuration += t
+
+    @property
+    def fps(self):
+        if not self.count:
+            return 0
+        return 1.0 / self.mean_duration
+
+    @property
+    def mean_duration(self):
+        if not self.count:
+            return 0.0
+        return self.totalDuration / self.count
+
+    def reset(self):
+        self.count = 0
+        self.totalDuration = 0
+        self.maxTime = 0
+        self.minTime = 0
+
+    def __str__(self):
+        return "Mean: {:4.4f}, FPS: {:8.1f}, N: {:5d} T: {:4.4f}".format(self.mean_duration, self.fps, self.count, self.totalDuration)
 
 class InferenceWrapper:
   def __init__(self, model_path):
     graph_def = tf.GraphDef()
+    self.detection_timer = CodeBlockTimer(10)
     with tf.gfile.FastGFile(model_path, "rb") as f:
       graph_def.ParseFromString(f.read())
     self.image_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, None, None, 3))
@@ -85,9 +129,10 @@ class InferenceWrapper:
       model_output_mapping['detection_scores'],
       model_output_mapping['detection_classes']
     ]
-
-    self.results = sess.run(fetches=dict(zip(fetches, [f + ':0' for f in fetches])),
-                            feed_dict={self.image_placeholder: encoded_image})
+    with self.detection_timer:
+      self.results = sess.run(fetches=dict(zip(fetches, [f + ':0' for f in fetches])),
+                              feed_dict={self.image_placeholder: encoded_image})
+    print('Detection', self.detection_timer)
     initial_state = self.results[model_output_mapping['lstm/initial_state']]
     return initial_state
 
@@ -108,8 +153,7 @@ with tf.Session() as sess:
   generator = caption_generator.CaptionGenerator(model, vocab)
 
   cap = cv2.VideoCapture(args.source)
-
-  last_fps = 0
+  total_timer = CodeBlockTimer(10)
 
   while True:
     t0 = time()
@@ -119,18 +163,13 @@ with tf.Session() as sess:
     
     frame = cv2.resize(frame, tuple(int(x) for x in args.size.split('x')))
     frame_f = (np.expand_dims(frame, 0) / 255.0).astype(np.float32)
-
-    captions = generator.beam_search(sess, frame_f)
-    fps = 1.0 / (time() - t0)
-    if abs(fps - last_fps)/fps > 0.1: 
-      last_fps = 0
-    last_fps += fps
-    last_fps /= 2.0
+    with total_timer:
+      captions = generator.beam_search(sess, frame_f)
     spaceing = 15
     text_pos = 0
     text_pos += spaceing
     cv2.putText(frame,
-                "FPS: {}".format(int(last_fps * 100) / 100.0),
+                "FPS: {}".format(int(total_timer.fps * 100) / 100.0),
                 (0, text_pos),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.4,
@@ -168,6 +207,7 @@ with tf.Session() as sess:
                   (255, 0, 0))
 
     cv2.imshow('window', frame)
+    print('Total', total_timer)
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
